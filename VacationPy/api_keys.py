6c775bccd7b9041a4503352ce90c65b00c2ce57d
David <davidscottsbu@gmail.com>
2024-08-11 12:00:59 -0500

    second commit

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "9f1cb1a5e9944ce539716066894e398e"
+
+# Geoapify API Key
+geoapify_key = "6a9b051655cd4af8b5c5b74773d2c734"
--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "9f1cb1a5e9944ce539716066894e398e"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "6a9b051655cd4af8b5c5b74773d2c734"
